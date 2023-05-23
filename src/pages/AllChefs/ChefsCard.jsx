527c74f9f275788929bc94878418608d621708fc
kawsaruhb <kawsaruhb@gmail.com>
2023-05-24 02:25:00 +0600

    chefsCard updated

--- src/pages/AllChefs/ChefsCard.jsx
+++ src/pages/AllChefs/ChefsCard.jsx
@@ -7,15 +7,15 @@ const ChefsCard = ({ chefs }) => {
 
     return (
         <div>
-            <div className="card w-96 p-4 glass bg-red-100">
-                <figure><img className='w-full h-80 rounded-lg' src={chef_img} alt="car!" /></figure>
+            <div className="card w-80 p-4 glass bg-red-100">
+                <figure><img className='w-full h-68 rounded-lg' src={chef_img} alt="car!" /></figure>
                 <div className="mt-2">
                     <h2 className="card-title text-black font-bold text-2xl">{name}</h2>
                     <p className='text-lg text-black mt-1'>Experience: {experience}</p>
                     <p className='text-lg text-black mt-1'>Recipes: {nOf_recipes}</p>
                     <p className='flex gap-2 text-black text-lg mt-1'> <FaThumbsUp className='mt-1 mx-1 text-blue-500' /> {likes}</p>
                     <div className="card-actions justify-end">
-                        <Link to={`/chef-detail/${_id}`}><button className="btn btn-primary bg-[#A53d02] border-none w-50">Recipes</button></Link>
+                        <Link to={`/chef-detail/${_id}`}><button className="btn btn-error font-bold border-none w-full">Recipes</button></Link>
                     </div>
                 </div>
             </div>
