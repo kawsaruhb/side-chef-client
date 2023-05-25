7eebd6461cdf083704c7a96835644bf17f4fc5f0
kawsaruhb <kawsaruhb@gmail.com>
2023-05-26 01:04:58 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrCpNp7PPiAoU_b9dj6xdETGHsRAQ8hWk",
-  authDomain: "side-chef-client.firebaseapp.com",
-  projectId: "side-chef-client",
-  storageBucket: "side-chef-client.appspot.com",
-  messagingSenderId: "241543453914",
-  appId: "1:241543453914:web:26c746719ec3d2cb5219e3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
